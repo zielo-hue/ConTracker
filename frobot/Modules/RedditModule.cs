e80edc2608df50fca0ce520692c76f677b95a1ea
kamasu <invalid@dakota>
2019-11-22 07:25:01 -0800

    ..

--- frobot/Modules/RedditModule.cs
+++ frobot/Modules/RedditModule.cs
@@ -29,7 +29,7 @@ namespace frobot.Modules
                 .WithColor(Color.Azure)
                 .Build());
 
-        [Command("rtf2top")]
+        [Command("top", "hot")]
         public async Task RedditAsync()
         {
             var topPost = tf2.Posts.Hot[rnd.Next(0, 15)];
@@ -44,6 +44,27 @@ namespace frobot.Modules
                 .WithImageUrl(((LinkPost)topPost).URL)
                 .WithColor(Color.Azure)
                 .Build()).ConfigureAwait(true);
+        }
+
+        [Group("reddit")]
+        public class GetModule : DiscordModuleBase
+        {
+            [Command("hot", "top")]
+            public async Task RedditTop()
+            {
+                var topPost = tf2.Posts.Hot[rnd.Next(0, 15)];
+                var selftext = topPost.Listing.IsSelf ? ((SelfPost)topPost).SelfText : "(linkpost)";
+                await ReplyAsync(embed: new LocalEmbedBuilder()
+                    .WithTitle(topPost.Title)
+                    .WithUrl("https://www.reddit.com/r/tf2/comments/" + topPost.Id)
+                    .WithDescription(selftext)
+                    .AddField("Author", value: "u/" + topPost.Author, true)
+                    .AddField("Upvotes", value: topPost.UpVotes.ToString(), true)
+                    .AddField("Id: ", value: topPost.Id, true)
+                    .WithImageUrl(((LinkPost)topPost).URL)
+                    .WithColor(Color.Azure)
+                    .Build()).ConfigureAwait(true);
+            }
         }
     }
 }
