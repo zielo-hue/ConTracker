7e8b4134ac055bc96626b746e2b8264630793e06
jolly <nyoom@car>
2020-01-07 08:50:18 -0800

    fix logger ja??

--- frobot/Program.cs
+++ frobot/Program.cs
@@ -1,48 +1,39 @@
 ﻿using System;
-using System.Linq;
 using System.IO;
-using System.Reflection;
-using System.Threading.Tasks;
 using Disqord;
 using Disqord.Bot;
-using Disqord.Rest;
+using Disqord.Bot.Sharding;
+using Disqord.Bot.Prefixes;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Linq;
 
 namespace frobot
 {
-    internal sealed class Program
+    internal sealed class Program : DiscordBotSharder
     {
-        private static void Main(string[] args)
-        {
-            new Program().MainAsync().GetAwaiter().GetResult();
-        }
-
-        private async Task MainAsync()
-        {
-            var conf = JObject.Parse(File.ReadAllText(@"config.json"));
-            var token = (string) conf["DISCORD"]["TOKEN"];
-            using var bot = new DiscordBot(TokenType.Bot, token, new DiscordBotConfiguration
+        static JObject conf = JObject.Parse(File.ReadAllText(@"config2.json"));
+        static String token = (string) conf["DISCORD"]["TOKEN"];
+        private static void Main()
+            => new Program().Run();
+        
+        private Program() : base(TokenType.Bot, token,
+            new DefaultPrefixProvider()
+                .AddPrefix("~~")
+                .AddMentionPrefix(),
+            new DiscordBotConfiguration
             {
-                Prefixes = new[] { "!!" },
+                Status = UserStatus.Invisible,
                 ProviderFactory = bot => new ServiceCollection()
-                .AddSingleton((DiscordBot)bot)
-                .AddSingleton(new Services.RedditService((DiscordBot) bot))
-                .BuildServiceProvider()
-            });
-            bot.Logger.MessageLogged += this.Logger_MessageLogged;
-            bot.AddModules(Assembly.GetExecutingAssembly());
-
-            var guild = await bot.GetGuildAsync(535572626480037909);
-            var channels = await guild.GetChannelsAsync();
-            var channel = channels.OfType<RestTextChannel>().FirstOrDefault(x => x.Name == "general");
-
-            bot.Run();
-        }
-
-        private void Logger_MessageLogged(object sender, Disqord.Logging.MessageLoggedEventArgs e)
+                    .AddSingleton((DiscordBot) bot)
+                    .AddSingleton(new Services.RedditService((DiscordBot) bot))
+                    .BuildServiceProvider()
+            })
         {
-            Console.WriteLine(e);
+            Logger.MessageLogged += MessageLogged;
+            AddModules(typeof(Program).Assembly);
         }
+
+        private void MessageLogged(object sender, Logger.MessageLoggedEventArgs e)
+            => Console.WriteLine(e);
     }
 }
