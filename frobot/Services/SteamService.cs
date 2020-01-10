e6e3356f1be74ce65205e43fee9fb6ecf373745a
jolly <nyoom@car>
2020-01-10 15:25:25 -0800

    TODO just switch to GetNewsForApp instead because for some reason the schema for SteamVR is broken, thanks valve pls fix

--- frobot/Services/SteamService.cs
+++ frobot/Services/SteamService.cs
@@ -2,6 +2,9 @@
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Disqord;
+using Disqord.Bot.Sharding;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Steam.Models;
 using SteamWebAPI2.Interfaces;
@@ -11,10 +14,14 @@ namespace frobot.Services
 {
     public class SteamService
     {
-        private readonly JObject _conf = JObject.Parse(File.ReadAllText(@"config2.json"));
+        private static JObject _conf = JObject.Parse(File.ReadAllText(@"config2.json"));
+        
+        // Temporary
+        private readonly DiscordBotSharder _bot;
 
-        public SteamService()
+        public SteamService(DiscordBotSharder bot)
         {
+            _bot = bot;
             var webInterfaceFactory = new SteamWebInterfaceFactory((string) _conf["STEAM"]["KEY"]);
             var steamInterface = webInterfaceFactory.CreateSteamWebInterface<SteamUserStats>(new HttpClient());
 
@@ -22,13 +29,37 @@ namespace frobot.Services
         }
         
         /*
-         * TODO: Write check with version in the json and wire it up with RedditService
+         * TODO: Wire it up with RedditService, get working SteamVR SteamAPI schema format
          */
         async Task CheckAppVersions(SteamUserStats steamInterface)
         {
-            var gameSchema = steamInterface
-                .GetSchemaForGameAsync(250820, "English");
-            var gameVersion = gameSchema.Result.Data.GameVersion;
+            while (true)
+            {
+                // remember to change this to 1 minute
+                await Task.Delay(10000).ConfigureAwait(true);
+                
+                var gameSchema = steamInterface
+                    .GetSchemaForGameAsync(440);
+                var gameVersion = gameSchema.Result.Data.GameVersion;
+
+                Console.WriteLine(gameVersion);
+
+                _conf = JObject.Parse(File.ReadAllText(@"config2.json"));
+                
+                if ((string) _conf["latestAppVersion"] == gameVersion)
+                {
+                    await _bot.SendMessageAsync(558044111421177866, "app version check, no new changes")
+                        .ConfigureAwait(true);
+                }
+                else if ((string) _conf["latestAppVersion"] != gameVersion)
+                {
+                    await _bot.SendMessageAsync(558044111421177866, 
+                            "app version check, new changes detected: local " + (string) _conf["latestAppVersion"] + " remote "+ gameVersion)
+                        .ConfigureAwait(true);
+                    _conf["latestAppVersion"] = gameVersion;
+                    await File.WriteAllTextAsync(@"config2.json", _conf.ToString()).ConfigureAwait(true);
+                }
+            }
         }
     }
 }
