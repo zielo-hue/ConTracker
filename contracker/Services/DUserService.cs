14c5bac1e4a5b567cc1af6dde22e2584d4e8b136
kamasu <invalid@dakota>
2020-01-24 22:59:06 -0800

    goodbye reddit reta

--- contracker/Services/DUserService.cs
+++ contracker/Services/DUserService.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Disqord;
+using Disqord.Bot.Sharding;
 using Qmmands;
 
 namespace contracker.Services
@@ -9,10 +10,23 @@ namespace contracker.Services
     public class DUserService
     {
         private DiscordClient client;
+        private readonly DiscordBotSharder _bot;
         
-        public DUserService(string token)
+        public DUserService(DiscordBotSharder bot, string token)
         {
+            _bot = bot;
             client = new DiscordClient(TokenType.User, token);
+            _ = UpdatePresence();
+        }
+        
+        // i forgot to do this
+        async Task UpdatePresence()
+        {
+            while (true)
+            {
+                await Task.Delay(30000);
+                await _bot.SetPresenceAsync(new LocalActivity("FUCK holonoid & gaming", ActivityType.Playing));
+            }
         }
 
         // Use steam API
