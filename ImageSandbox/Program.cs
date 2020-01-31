5427b939082595c7498b0ba69b98e43edd27de0c
jolly <nyoom@car>
2020-01-30 18:33:09 -0800

    byebye

--- ImageSandbox/Program.cs
+++ ImageSandbox/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.IO;
+using SixLabors.Fonts;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace ImageSandbox
 {
@@ -7,7 +10,31 @@ namespace ImageSandbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var output = new MemoryStream();
+            using (var template = Image.Load("whatthefuck.jpg"))
+            {
+                Font font = SystemFonts.CreateFont("Arial", 10);
+
+                using (var img = template.Clone(ctx => ctx.ApplyScalingWaterMark()))
+                {
+                    img.Save("bruh.jpg");
+                }
+            }
         }
+
+        private static IImageProcessingContext ApplyScalingWaterMark(this IImageProcessingContext processingContext,
+            Font font,
+            string text,
+            Color color,
+            float padding,
+            bool wordwrap)
+        {
+            if (wordwrap)
+                return processingContext.ApplyScalingWaterMarkWordWrap(font, text, color, padding);
+            else
+                return processingContext.ApplyScalingWaterMarkSimple(font, text, color, padding);
+        }
+        
+        
     }
 }
